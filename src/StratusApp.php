d305ece60e5993ec592e2b6f6dd09f04f695d8cc
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-11-02 09:03:07 -0500

    Temp

--- src/StratusApp.php
+++ src/StratusApp.php
@@ -12,35 +12,8 @@ use ThenLabs\ComposedViews\Asset\Script;
  */
 class StratusApp extends AbstractAppWithSElements
 {
-    public function __construct(string $controllerUri)
-    {
-        parent::__construct($controllerUri);
-
-        $this->addFilter([$this, '_addCustomScript']);
-    }
-
     public function getView(array $data = []): string
     {
         return file_get_contents(__DIR__.'/../templates/index.html');
-    }
-
-    public function _addCustomScript(RenderEvent $event): void
-    {
-        $script = new Script('easy-mails', null, '');
-        $script->setSource(<<<JAVASCRIPT
-            setInterval(() => {
-                stratusAppInstance.dispatch('update', {}, false);
-            }, 1000);
-        JAVASCRIPT);
-
-        $event->filter('body')->append($script->render());
-    }
-
-    public function onClickBody(): void
-    {
-        for ($i = 1; $i <= 5; $i++) {
-            $this->contentTitle->textContent .= $this->contentTitle->textContent;
-            sleep(1);
-        }
     }
 }
